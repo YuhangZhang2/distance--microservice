1755fa1617fa856d0895e6537f867be5cba6abae
Yuhang Zhang <zhanyuh2@oregonstate.edu>
2022-05-09 14:58:24 -0700

    Update distance.js

--- service/distance.js
+++ service/distance.js
@@ -5,7 +5,7 @@ const zipCodeURL = 'https://www.zipcodeapi.com/rest/';
 
 var distance = {
     find: function(req, res, next) {
-        request(zipCodeURL + apiKey + '/distance.json/' + req.params.zipcode1 + '_' + req.params.zipcode2 + '/mile',
+        request(zipCodeURL + apiKey + '/distance.json/' + req.params.zipcode1 + '/' + req.params.zipcode2 + '/mile',
         function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 response = JSON.parse(body);
